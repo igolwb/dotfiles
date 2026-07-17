fe22f44d09811ead8dae63b883a85b047efcaa3d
@_Ingoat <igolwb@gmail.com>
2026-07-16 23:19:53 -0300

    Comment out monitor settings

--- dot_config/hypr/config/monitors.lua
+++ dot_config/hypr/config/monitors.lua
@@ -2,9 +2,9 @@
 -- @*** is the refresh rate for the monitor,
 -- do not set it over the value that your monitor uses
 
-hl.monitor({
-    output   = "",
-    mode     = "1920x1080@100",
-    position = "auto",
-    scale    = "auto",
-})
+-- hl.monitor({
+-- output   = "",
+--  mode     = "1920x1080@100",
+--  position = "auto",
+--  scale    = "auto",
+--})
