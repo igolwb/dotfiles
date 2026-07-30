f6e61758fb75af1f84ff1aa024ef9e502ba92794
@_Ingoat <igolwb@gmail.com>
2026-07-30 17:12:05 -0300

    fix gaming workspace

--- dot_config/hypr/config/windowrules.lua
+++ dot_config/hypr/config/windowrules.lua
@@ -12,27 +12,34 @@ hl.window_rule({
 local gamingApps = "^(steam_app.*|gamescope)$"
 local gamingWorkspace = "name:game"
 
+
 hl.window_rule({ match = { content = "game" }, workspace = gamingWorkspace })
+hl.window_rule({ match = { xdg_tag = "^(.*game.*)$" }, workspace = gamingWorkspace, fullscreen_state = 2, content = "game", sync_fullscreen = true })
 hl.window_rule({ match = { class = gamingApps }, workspace = gamingWorkspace })
 hl.window_rule({ match = { class = "^(steam)$", title = "^(Friends List)$" }, float = true })
+hl.window_rule({ match = { class = "^(steam)$", title = "^(Launching\\.{3})$" }, float = true, center = true, workspace = gamingWorkspace })
 hl.window_rule({
-	match = {
-		class = "^(steam)$",
-		title = "^(Launching\\.{3})$",
-	},
-	float = true,
-	center = true,
-	workspace = gamingWorkspace,
+    match = {
+        class         = gamingApps,
+        title         = "^(.+)$",
+        initial_title = "negative:^(.*\\\\home\\\\.*)$",
+    },
+    content          = "game",
+    decorate         = false,
+    fullscreen_state = 2,
+    size             = { "monitor_w", "monitor_h" },
+    sync_fullscreen  = true,
 })
 hl.window_rule({
-	match = {
-		class = gamingApps,
-		title = "^(.+)$",
-		initial_title = "negative:^(.*\\\\home\\\\.*)$",
-	},
-	size = "monitor_w monitor_h",
-	fullscreen_state = 2,
-	content = "game",
+    match = {
+        class         = "^(steam_app.*)$",
+        initial_title = "^$",
+    },
+    center           = true,
+    float            = true,
+    fullscreen       = false,
+    fullscreen_state = 0,
+    workspace        = gamingWorkspace,
 })
 hl.window_rule({
 	match = {
@@ -125,3 +132,4 @@ hl.layer_rule({
 	blur = true,
 	blur_popups = true,
 })
+
